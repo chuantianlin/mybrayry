af4c6aabcc3a974ca9b3bd623bc3fdb59cda4f2b
chuantianlin <45149972+chuantianlin@users.noreply.github.com>
2022-03-25 20:52:05 -0400

    book created index/new

--- routes/books.js
+++ routes/books.js
@@ -16,9 +16,27 @@ const upload = multer({
 
 // All Books Route
 router.get('/', async (req, res) => {
- 
-    res.send('Hello world')
-  
+  let query=Book.find()
+  if (req.query.title!=null && req.query.title!=''){
+    query=query.regex('title',new RegExp(req.query.title,'i'))
+  if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
+      query = query.lte('publishDate', req.query.publishedBefore)
+    }
+  if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
+      query = query.gte('publishDate', req.query.publishedAfter)
+    }
+  }
+  try{
+      const books=await Book.find({})
+      res.render('books/index',
+      {
+        books:books,
+        searchOptions:req.query
+      })
+  } catch{
+    res.redirect('/')
+  }
+
 })
 
 // New Book Route
